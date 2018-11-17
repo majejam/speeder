4e4dc6a139b115ffacf8d3c8d95b27eba09ada6f
Thomas_main <t.f.m.lacroix@gmail.com>
2018-11-17 16:51:09 +0100

    re-organized functions

--- js/dom_handler.js
+++ js/dom_handler.js
@@ -34,6 +34,16 @@ function optionShow(){
     playingState = true
   }
 }
+document.getElementById('Play').addEventListener('click', function() {
+	resetLevel()
+	resetPlayer()
+	if(autoRun == false){
+		autoRun = true
+	}
+	else{
+		autoRun = false
+	}
+}, false)
 returnHome.addEventListener('click', () => {
   homeContainer.classList.toggle('menu-container-unselected')
 })
@@ -54,4 +64,78 @@ function generateStarsMenu(starContainer) {
   starElement.style.animationDelay =`${random}ms`;
   starElement.innerHTML = ' ';
   starContainer.appendChild(starElement)
+}
+
+
+
+
+//game options
+function getSeedLevel(){
+  let seed = document.getElementById('seed').value
+  return seed
+}
+function getSizeElement(){
+  let size = parseInt(document.getElementById('size').value)
+  return size
+}
+function getNumberOfElement(){
+  let numberElements = parseInt(document.getElementById('numberElements').value)
+  if(numberElements > 1000){
+    numberElements = 1000
+  }
+  if(numberElements < 0){
+    numberElements = 1
+  }
+  if( isNaN(numberElements) ){
+    numberElements = 1
+  }
+  setNumberOfElement(numberElements)
+  return numberElements
+}
+function getNumberOfSpacing(){
+  let numberSpacing = parseInt(document.getElementById('numberSpacing').value)
+  return numberSpacing
+}
+function setNumberOfSpacing(element){
+  document.getElementById('numberSpacing').value = element
+}
+function setNumberOfElement(element){
+  document.getElementById('numberElements').value = element
+}
+function setSizeElement(element){
+  document.getElementById('size').value = element
+}
+function setSeedLevel(element){
+  document.getElementById('seed').value = element
+}
+document.getElementById('manualGenerate').addEventListener('click', function() {
+  resetCanvas()
+  if(autoGenerate == true){
+    console.clear()
+    setElementInDOM()
+  }
+  traps = generateTraps(autoGenerate, getNumberOfElement(),getSizeElement(),getNumberOfSpacing())
+}, false)
+
+
+
+document.getElementById('autoGenerate').addEventListener('click', function() {
+  if(autoGenerate == true){
+    document.getElementById('autoGenerate').classList.toggle('unselected')
+    document.getElementById('autoGenerate').classList.toggle('selected')
+      console.log("Generation is manual")
+    autoGenerate = false
+  }else{
+    autoGenerate = true
+    document.getElementById('autoGenerate').classList.toggle('unselected')
+    document.getElementById('autoGenerate').classList.toggle('selected')
+      console.log("Generation is automatic")
+  }
+}, false)
+
+function setElementInDOM(){
+  setSeedLevel(Math.ceil(Math.random()*10000))
+  setSizeElement(0.5 + Math.ceil(Math.random()*200)/100)
+  setNumberOfElement(Math.ceil(Math.random()*200))
+  setNumberOfSpacing(50 + Math.ceil(Math.random()*2000))
 }
